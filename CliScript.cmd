a5fc72da5cdea5724ddd254d3ac127d909cd9f7b
Maria Gavrikova <maria.gavrikova@gmail.com>
2018-10-15 02:10:10 +0300

    Updated CLI script

--- CliScript.cmd
+++ CliScript.cmd
@@ -2,6 +2,7 @@ set resourceGroup=adventure-works-rg-1
 set gatewayName=aw-app-gtw
 set gatewayIp=aw-app-gtw-ip
 set loadBalancerName=aw-vnet-gateway
+set loadBalancerIp=aw-vnet-gateway-ip
 set vnetName=aw-net
 set loadBalancerDns=aw-lb
 set loadBalancerProbe=sql-probe
@@ -17,7 +18,7 @@ echo "delete old gateway IP"
 call az network public-ip delete -g %resourceGroup% -n %gatewayIp%
 
 echo "create new load balancer points to SQL server VM"
-call az network lb create -g %resourceGroup% -n %loadBalancerName% --sku Basic --vnet-name %vnetName% --public-ip-address-allocation Static --public-ip-dns-name %loadBalancerDns%
+call az network lb create -g %resourceGroup% -n %loadBalancerName% --sku Basic --vnet-name %vnetName% --public-ip-address %loadBalancerIp% --public-ip-address-allocation Static --public-ip-dns-name %loadBalancerDns%
 
 echo "create health probe for SQL Server port"
 call az network lb probe create -g %resourceGroup% --lb-name %loadBalancerName% -n %loadBalancerProbe% --protocol %sqlProtocol% --port %sqlPort%
