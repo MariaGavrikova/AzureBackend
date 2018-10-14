995c5bd525f0e98814c0d5279ad6d25b91a48e81
Maria Gavrikova <maria.gavrikova@gmail.com>
2018-10-15 01:55:48 +0300

    Updated CLI script

--- CliScript.cmd
+++ CliScript.cmd
@@ -1,17 +1,26 @@
 set resourceGroup=adventure-works-rg-1
 set gatewayName=aw-app-gtw
+set gatewayIp=aw-app-gtw-ip
+set loadBalancerName=aw-vnet-gateway
+set vnetName=aw-net
+set loadBalancerDns=aw-lb
+set loadBalancerProbe=sql-probe
+set loadBalancerRule=default
+set sqlProtocol=tcp
+set sqlPort=1433
+set loadBalancerPool=sql-pool
 
-echo "create new backend pool that points to SQL server VM"
-call az network application-gateway address-pool create -g %resourceGroup% --gateway-name %gatewayName% -n sql-pool --servers 10.0.1.7
+echo "delete old application gateway"
+call az network application-gateway delete -g %resourceGroup% -n %gatewayName%
 
-echo "add new http setting"
-call az network application-gateway http-settings create -g %resourceGroup% --gateway-name %gatewayName% -n sql-http-settings --port 1433 --protocol Http --cookie-based-affinity Disabled --timeout 30
+echo "delete old gateway IP"
+call az network public-ip delete -g %resourceGroup% -n %gatewayIp%
 
-echo "add new port"
-call az network application-gateway frontend-port create -g %resourceGroup% --gateway-name %gatewayName% -n sql-1433 --port 1433
+echo "create new load balancer points to SQL server VM"
+call az network lb create -g %resourceGroup% -n %loadBalancerName% --sku Basic --vnet-name %vnetName% --public-ip-address-allocation Static --public-ip-dns-name %loadBalancerDns%
 
-echo "add new listener"
-call az network application-gateway http-listener create -g %resourceGroup% --gateway-name %gatewayName% --frontend-port sql-1433 -n sql-listener --frontend-ip appGatewayFrontendIp
+echo "create health probe for SQL Server port"
+call az network lb probe create -g %resourceGroup% --lb-name %loadBalancerName% -n %loadBalancerProbe% --protocol %sqlProtocol% --port %sqlPort%
 
-echo "add new rule"
-call az network application-gateway rule create -g %resourceGroup% --gateway-name %gatewayName% -n sql-rule --http-listener sql-listener --rule-type Basic --address-pool sql-pool --http-settings sql-http-settings
+echo "create load balancer backend pool and rule"
+call az network lb rule create -g %resourceGroup% --lb-name %loadBalancerName% -n %loadBalancerRule% --protocol %sqlProtocol% --frontend-port %sqlPort% --backend-port %sqlPort% --probe-name %loadBalancerProbe%
