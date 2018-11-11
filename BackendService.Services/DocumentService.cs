9aba073253d42d71ecd5279f1609c146a3555e60
Maria Gavrikova <maria.gavrikova@gmail.com>
2018-11-11 22:58:03 +0300

    Updated deployment settings.

--- BackendService.Services/DocumentService.cs
+++ BackendService.Services/DocumentService.cs
@@ -14,11 +14,15 @@ namespace BackendService.Services
 {
     public class DocumentService
     {
-        private readonly Uri _blobUrl = new Uri("https://adventureworksrg1diag866.blob.core.windows.net/adventure-works-documents-blob");
-        private const string StorageAccount = "adventureworksrg1diag866";
-        private const string AccessKey = "mvq1tXXtIxyA46z8xOHGrYIGz1uN2seB/jwgdoOwMPpPV8VyGSL0S92R1GsGTwsZx8JB7+SEqAumvmBPwEzJDw==";
+        private readonly Uri _blobUrl = new Uri($"https://{StorageAccount}.blob.core.windows.net/{Blob}");
+        //private const string StorageAccount = "adventureworksrg1diag866";
+        private const string StorageAccount = "adventureworksrg1diag867";
+        //private const string AccessKey = "mvq1tXXtIxyA46z8xOHGrYIGz1uN2seB/jwgdoOwMPpPV8VyGSL0S92R1GsGTwsZx8JB7+SEqAumvmBPwEzJDw==";
+        private const string AccessKey = "l9nckJx7Ki6+DihJMAr3Bp85EB8OHohxPet8W9gmt/xdczv+pQV35WCV1qB134aMNAeWIFHbzMk9Eh7901xbfQ==";
         private readonly string StorageConnectionString = 
             $"DefaultEndpointsProtocol=https;AccountName={StorageAccount};AccountKey={AccessKey};EndpointSuffix=core.windows.net";
+        private const string Blob = "adventure-works-documents-blob";
+        private const string Queue = "adventure-works-documents-queue";
         private CloudBlobClient _blobClient;
         private CloudQueueClient _queueClient;
 
@@ -33,11 +37,11 @@ namespace BackendService.Services
         public async Task SaveAsync(Stream document, string fileName, long size)
         {
             var fileId = Guid.NewGuid().ToString();
-            var container = _blobClient.GetContainerReference("adventure-works-documents-blob");
+            var container = _blobClient.GetContainerReference(Blob);
             var blob = container.GetBlockBlobReference(fileId);
             await blob.UploadFromStreamAsync(document);
 
-            var queue = _queueClient.GetQueueReference("adventure-works-documents-queue");
+            var queue = _queueClient.GetQueueReference(Queue);
             queue.CreateIfNotExists();
 
             var stringBuilder = new StringBuilder();
